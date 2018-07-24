97523827635d82359aa706c979ff0c5bb970dc79
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-24 16:54:54 -0600

    Implements Login Button and Landing Page, in monolith not components. Pre-refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
 class App extends Component {
   constructor() {
@@ -9,6 +13,14 @@ class App extends Component {
     this.state = {
       isLoggedIn: false
     }
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin() {
+    this.setState(() => ({
+      isLoggedIn: true
+    }));
   }
 
 
@@ -16,9 +28,30 @@ class App extends Component {
     let page;
     if (!this.state.isLoggedIn) {
       page = (
-        <Grid item xs={12}>
-          <Button variant='contained'>Log In With Lyft</Button>
-        </Grid>
+        <Button onClick={this.handleLogin}
+                variant='contained'>Log In With Lyft</Button>
+      );
+    } else {
+      page = (
+        <div>
+          <AppBar>
+            <Toolbar>
+              <Grid container spacing={12}>
+                <Grid item xs={10}>
+                  <h1 style={{ fontSize: 22, textAlign: 'left' }}>Magic Carpet</h1>
+                </Grid>
+                <Grid item xs={1}>
+                  <SettingsApplicationsIcon style={{ fontSize: 40 }} />
+                </Grid>
+                <Grid item xs={1}>
+                  <AccountCircleIcon style={{ fontSize: 40 }} />
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+
+          <Button variant='contained'>Magic Carpet</Button>
+        </div>
       );
     }
 
