6175ab84dbe2b98e4e5094da58b9ffa5d893b371
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-10-01 12:40:56 -0600

    Create Sponsor component footer

---
+++ src/components/Sponsor.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sponsor = () => {
+	return (
+		<div className='sponsor-banner'>
+			<p className='sponsor-tagline'>
+				powered by 
+				<img className='sponsor-logo' alt='lyft-logo' src='/images/lyft-logo.png'/>
+				and
+				<img className='sponsor-logo yelp-logo' alt='yelp-logo' src='/images/yelp-logo.png'/>
+			</p>
+		</div>
+	)
+}
+
+export default Sponsor
